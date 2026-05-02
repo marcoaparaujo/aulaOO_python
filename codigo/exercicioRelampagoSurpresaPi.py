df66f657ddfafcb3a8fb0f7bb6d1fdebd56feef2
Marco Antonio Araujo <marcoaparaujo@gmail.com>
2026-05-02 10:35:00 -0300

    Add classes for education types, schools, and courses

--- codigo/exercicioRelampagoSurpresaPi.py
+++ codigo/exercicioRelampagoSurpresaPi.py
@@ -1,3 +1,10 @@
+class TipoEnsino:
+    def __init__(self):
+        self.nome = ""
+    def set_nome(self, nome):
+        self.nome = nome
+    def get_nome(self):
+        return self.nome
 class Estado:
     def __init__(self):
         self.nome = ""
@@ -9,17 +16,23 @@ class Estado:
 class Cidade:
     def __init__(self, estado):
         self.set_estado(estado)
+        self.nome = ""
     def get_estado(self):
         return self.estado
     def set_estado(self, estado):
         if estado != None:
             self.estado = estado
+    def get_nome(self):
+        return self.nome
+    def set_nome(self, nome):
+        self.nome = nome
     def get_nome_estado(self):
         return self.estado.get_nome()
 class Pessoa:
     def __init__(self):
         self.escolaridade = None
         self.naturalidade = None
+        self.nome = ""
     def set_escolaridade(self, escolaridade):
         self.escolaridade = escolaridade
     def get_escolaridade(self):
@@ -28,6 +41,10 @@ class Pessoa:
         self.naturalidade = naturalidade
     def get_naturalidade(self):
         return self.naturalidade
+    def set_nome(self, nome):
+        self.nome = nome
+    def get_nome(self):
+        return self.nome
     def get_descricao_escolaridade(self):
         if self.escolaridade == None:
             return "Pessoa sem escolaridade"
@@ -38,11 +55,35 @@ class Pessoa:
             return "Pessoa sem naturalidade"
         else:
             return self.naturalidade.get_nome_estado()
-
+    def get_nome_cidade_naturalidade(self):
+        if self.naturalidade == None:
+            return "Pessoa sem naturalidade"
+        else:
+            return self.naturalidade.get_nome()
 
 class Professor (Pessoa):
     def __init__(self):
         super().__init__()
+        self.curso = None
+    def set_curso(self, curso):
+        self.curso = curso
+    def get_curso(self):
+        return self.curso
+    def get_nome_tipo_ensino(self):
+        if self.curso == None:
+            return "Professor sem curso"
+        else:
+            return self.curso.get_nome_tipo_ensino()
+    def get_nome_diretor(self):
+        if self.curso == None:
+            return "Professor sem curso"
+        else:
+            return self.curso.get_nome_diretor()
+    def get_nome_coordenador(self):
+        if self.curso == None:
+            return "Professor sem curso"
+        else:
+            return self.curso.get_nome_coordenador()
 
 class Aluno(Pessoa):
     def __init__(self, curso):
@@ -55,6 +96,8 @@ class Aluno(Pessoa):
             self.curso = curso
     def get_nome_estado(self):
         return self.curso.get_nome_estado()
+    def get_nome_coordenador(self):
+        return self.curso.get_nome_coordenador()
 
 class Escolaridade:
     def __init__(self):
@@ -65,9 +108,10 @@ class Escolaridade:
         self.descricao = descricao
 
 class Curso:
-    def __init__(self):
+    def __init__(self, tipo_ensino):
         self.coordenador = None
         self.escola = None
+        self.set_tipo_ensino(tipo_ensino)
     def get_coordenador(self):
         return self.coordenador
     def set_coordenador(self, coordenador):
@@ -76,6 +120,11 @@ class Curso:
         self.escola = escola
     def get_escola(self):
         return self.escola
+    def set_tipo_ensino(self, tipo_ensino):
+        if tipo_ensino != None:
+            self.tipo_ensino = tipo_ensino
+    def get_tipo_ensino(self):
+        return self.tipo_ensino
     def get_descricao_escolaridade_coordenador(self):
         if self.coordenador == None:
             return "Curso sem coordenador"
@@ -86,16 +135,43 @@ class Curso:
             return "Curso sem escola"
         else:
             return self.escola.get_nome_estado()
+    def get_nome_tipo_ensino(self):
+        return self.tipo_ensino.get_nome()
+    def get_nome_coordenador(self):
+        if self.coordenador == None:
+            return "Curso sem coordenador"
+        else:
+            return self.coordenador.get_nome()
+    def get_nome_diretor(self):
+        if self.escola == None:
+            return "Curso sem escola"
+        else:
+            return self.escola.get_nome_diretor()
 
 class Escola:
     def __init__(self):
         self.cidade = None
+        self.diretor = None
     def get_cidade(self):
         return self.cidade
     def set_cidade(self, cidade):
         self.cidade = cidade
+    def get_diretor(self):
+        return self.diretor
+    def set_diretor(self, diretor):
+        self.diretor = diretor
     def get_nome_estado(self):
         if self.cidade == None:
             return "Escola sem cidade"
         else:
             return self.cidade.get_nome_estado()
+    def get_descricao_escolaridade_diretor(self):
+        if self.diretor == None:
+            return "Escola sem diretor"
+        else:
+            return self.diretor.get_descricao_escolaridade()
+    def get_nome_diretor(self):
+        if self.diretor == None:
+            return "Escola sem diretor"
+        else:
+            return self.diretor.get_nome()
